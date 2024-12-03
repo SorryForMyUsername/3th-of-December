d20c8b307c5c0aa7e3532771578071e22bd2b0bd
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-12-03 12:21:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -35,6 +35,43 @@ namespace Xml
             return omas;
         }
 
+        static int[] RandomArray(int length, int min, int max)
+        {
+            int[] omas = new int[length];
+            Random rnd = new Random();
+
+            for (int i = 0;i < length; i++)
+            {
+                omas[i] = rnd.Next(min, max);
+            }
+
+            return omas;
+        }
+
+        static int[] InitialArray()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            return array;
+        }
+
+        static int Average(int[] array)
+        {
+            int sum = 0;
+            foreach(int el in array)
+            {
+                sum += el;
+            }
+            return sum / array.Length;
+        }
+
+        static void OutputReverseArray(int[] array)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"array[{i}] = {array[i]}");
+            }
+        }
+
         static void OutputArray(int[] array)
         {
             foreach(int el in array)
@@ -43,13 +80,18 @@ namespace Xml
             }
         }
 
+        static void Output(string message)
+        {
+            Console.WriteLine(message);
+        }
+
         static void Main(string[] args)
         {
-            int size = Size();
-            int[] array = InputArray(size);
-            OutputArray(array);
+            int[] array = InitialArray();
+            OutputReverseArray(array);
 
-            Console.ReadKey();
+
+            Console.ReadKey(true);
         }
     }
 }
